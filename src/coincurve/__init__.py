ff375ce4ac551afc99f359da784ffceeda03203f
tecnovert <tecnovert@tecnovert.net>
2026-08-21 17:16:31 +0200

    build: update version

--- src/coincurve/__init__.py
+++ src/coincurve/__init__.py
@@ -2,7 +2,7 @@ from coincurve.context import GLOBAL_CONTEXT, Context
 from coincurve.keys import PrivateKey, PublicKey, PublicKeyXOnly
 from coincurve.utils import verify_signature
 
-__version__ = "21.0.3"
+__version__ = "21.0.4"
 __all__ = [
     "GLOBAL_CONTEXT",
     "Context",
